2ed8ac4cc99523c20d91ba46a9d3638c75b992c9
faizan <faizan.ah862@gmail.com>
2024-12-23 21:15:02 +0200

    update db

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ const pool = new Pool({
     await pool.query("SELECT 1");
     console.log("Database connected successfully");
   } catch (error) {
-    console.error("Error connecting to the database:", error.message);
+    console.error("Error connecting to the database:", error);
   }
 })();
 module.exports = pool;
